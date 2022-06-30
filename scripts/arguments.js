74be42997aa6bfd42e8f303ddca383e5a5227580
braulo <breuer.paulo@googlemail.com>
2022-06-30 22:17:43 +0200

    deploy fixes

---
+++ scripts/arguments.js
@@ -0,0 +1,8 @@
+module.exports = [
+  [
+    // admin addresses
+    "abc123",
+  ],
+  // required to sign
+  0,
+];
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,11 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const requiredToSign = 2;
+  const admins = (await ethers.getSigners()).map((acc) => acc.address);
+
   const Wallet = await ethers.getContractFactory("MultiSigWallet");
-  const wallet = await Wallet.deploy(
-    (await ethers.getSigners()).map((acc) => acc.address),
-    3
-  );
+  const wallet = await Wallet.deploy(admins, requiredToSign);
 
   await wallet.deployed();
 
